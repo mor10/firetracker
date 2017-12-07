3bcc104e8111439287144e166d3fd9d5a1459107
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2017-12-07 10:27:56 -0800

    Filter classes added.

--- development/JS/parser.js
+++ development/JS/parser.js
@@ -2,12 +2,75 @@
 const DATA = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRzGNwiFbhVzNzWQBViXBCYtL5NiU11pgcV3R5KNEza89zPvAWwAdEcSmSO9B-B2TToMT80U-s8cKqy/pub?output=csv";
 const MAIN = document.querySelector(".resource-list ul");
 
+// Location switch.
+// Outputs location class for filter:
+function parseLocation(data) {
+	switch(data) {
+		case 'All Fires':
+			return 'all ';
+			break;
+		case '#ThomasFire - Ventura, CA 201712':
+			return 'thomas ';
+			break;
+		case '#CreekFire - Los Angeles, CA 201712':
+			return 'creek ';
+			break;
+		case '#RyeFire - Los Angeles, CA 201712':
+			return 'rye ';
+			break;
+		case '#LittleMountainFire - San Bernadino, CA 201712':
+			return 'littlemountain ';
+			break;
+		case '#SkirballFire - Los Angeles, CA 201712':
+			return 'skirball ';
+			break;
+		default:
+			return 'other ';
+	}
+}
+
+// Resource type switch.
+// Outputs type class for filter:
+function parseType(data) {
+	switch(data) {
+		case 'Map':
+			return 'map';
+			break;
+		case 'News Stories and Unofficial Information':
+			return 'news';
+			break;
+		case 'Wind and Weather':
+			return 'weather';
+			break;
+		case 'Official Government Site':
+			return 'government';
+			break;
+		case 'Traffic and Transportation':
+			return 'traffic';
+			break;
+		case 'WebCams':
+			return 'webcams';
+			break;
+		case 'Twitter Feeds':
+			return 'twitter';
+			break;
+		case 'Mobile App':
+			return 'mobile';
+			break;
+		case 'Volunteer Resources':
+			return 'volunteer';
+			break;
+		default:
+			return 'other-type';
+	}
+}
+
 // Output HTML based on Drive sheet:
 function buildCards(data) {
 	console.log(data);
 	for(var i=0; i<data.length;i++) {
 		var item = document.createElement( 'li' );
-		var string = '<a href="' + data[i]['url'] + '">';
+		var string = '<a href="' + data[i]['url'] + '" class="' + parseLocation(data[i]['fire']) + parseType(data[i]['type']) +'">';
 		string += '<h4 class="name">' + data[i]['name'] + '</h4>';
 		string += '<div class="description">' + data[i]['description'] + '</div>';
 		string += '<div class="fire">Fire: <strong>' + data[i]['fire'] + '</strong></div>';
