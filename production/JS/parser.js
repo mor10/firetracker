ed13017a5a629049e6c025d4d6febaf72bcd5cf0
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2017-12-07 15:12:36 -0800

    Filters for location working.

--- production/JS/parser.js
+++ production/JS/parser.js
@@ -1,6 +1,60 @@
 // URL to Drive sheet:
 const DATA = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRzGNwiFbhVzNzWQBViXBCYtL5NiU11pgcV3R5KNEza89zPvAWwAdEcSmSO9B-B2TToMT80U-s8cKqy/pub?output=csv";
 const MAIN = document.querySelector(".resource-list ul");
+const FILTERS = document.querySelectorAll(".filter input");
+const ALL = document.querySelector('#all');
+const LOCATIONFILTERS = document.querySelectorAll(".filter.location input");
+
+
+function clearAll() {
+	if (ALL.checked) {
+		for (var i = 1 ; i < LOCATIONFILTERS.length; i++) {
+			if (LOCATIONFILTERS[i].checked) {
+				LOCATIONFILTERS[i].checked = false;
+			}
+		}
+	}
+}
+
+// Hide/show elements with the appropriate classes:
+function runFilter(filters) {
+	
+	var resources = document.querySelectorAll('.resource-list li a');
+	for (var i = 0 ; i < resources.length; i++) {
+
+		if (filters.includes('all')) {
+			resources[i].classList.remove('hide')
+			console.log('all');
+		} else {
+			// Get current classes and create array from classList:
+			var classes = [].slice.apply(resources[i].classList);
+
+			if (classes.includes(filters.join())) {
+				resources[i].classList.remove('hide')
+				console.log('remove');
+			} else {
+				resources[i].classList.add('hide');
+				console.log('add');
+			}
+		}
+
+	}
+}
+
+// What filters are selected?
+// Loop through filters and add any checked items to an array:
+function filterSelection() {
+	
+	var checked = [];
+	for (var i = 0 ; i < FILTERS.length; i++) {
+		if (FILTERS[i].checked) {
+			checked.push(FILTERS[i].getAttribute('value'));
+		}
+	}
+	
+	runFilter(checked);
+}
+
 
 // Location switch.
 // Outputs location class for filter:
@@ -67,10 +121,9 @@ function parseType(data) {
 
 // Output HTML based on Drive sheet:
 function buildCards(data) {
-	console.log(data);
 	for(var i=0; i<data.length;i++) {
 		var item = document.createElement( 'li' );
-		var string = '<a href="' + data[i]['url'] + '" class="' + parseLocation(data[i]['fire']) + parseType(data[i]['type']) +'">';
+		var string = '<a href="' + data[i]['url'] + '" class="' + parseLocation(data[i]['fire'])  +'">';
 		string += '<h4 class="name">' + data[i]['name'] + '</h4>';
 		string += '<div class="description">' + data[i]['description'] + '</div>';
 		string += '<div class="fire">Fire: <strong>' + data[i]['fire'] + '</strong></div>';
@@ -91,3 +144,11 @@ Papa.parse(DATA, {
 	}
 });
 
+// Listen for the All fires box:
+// ALL.addEventListener('change', clearAll, false);
+
+// Listen for change in filter selection. 
+// Trigger filter() on any interaction except the first (all) box:
+for (var i = 0 ; i < FILTERS.length; i++) {
+	FILTERS[i].addEventListener('change', filterSelection, false);
+}
