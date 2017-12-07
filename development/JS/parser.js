9e98b14cee9ebfa25bd1f20ccc99a15459b5bbd0
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2017-12-06 16:40:48 -0800

    Data set hooked in

--- development/JS/parser.js
+++ development/JS/parser.js
@@ -1,10 +1,30 @@
-// URL to Drive doc:
+// URL to Drive sheet:
 const DATA = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRzGNwiFbhVzNzWQBViXBCYtL5NiU11pgcV3R5KNEza89zPvAWwAdEcSmSO9B-B2TToMT80U-s8cKqy/pub?output=csv";
+const MAIN = document.querySelector(".resource-list ul");
+
+// Output HTML based on Drive sheet:
+function buildCards(json) {
+	console.log(json);
+	for(var i=0; i<json.length;i++) {
+		var item = document.createElement( 'li' );
+		var string = '<a href="' + json[i]['url'] + '">';
+		string += '<h4 class="name">' + json[i]['name'] + '</h4>';
+		string += '<div class="description">' + json[i]['description'] + '</div>';
+		string += '<div class="fire">Fire: <strong>' + json[i]['fire'] + '</strong></div>';
+		string += '<div class="type">Information type: <strong>' + json[i]['type'] + '</div>';
+		string += '</a>';
+		item.innerHTML = string;
+		MAIN.appendChild( item );
+	}
+	
+}
+
 
 // Parse CSV data into JSON
 // Source: https://gist.github.com/iwek/7154578
 function csvJSON(csv){    
-	var lines=csv.split("\n");
+	var lines=csv.split("\r");
+	
 	var result = [];
 	var headers=lines[0].split(",");
 	for(var i=1;i<lines.length;i++){
@@ -18,8 +38,9 @@ function csvJSON(csv){
 		result.push(obj);
 	}
 
-	//return result object
-	return JSON.stringify(result); 
+	//return result JSON object
+	console.log(JSON.stringify(result));
+	return JSON.parse(JSON.stringify(result)); 
 }
 
 // Send xhr request to DATA:
@@ -29,7 +50,8 @@ xhr.responseType = 'text';
 xhr.onload = function () {
 	if (xhr.readyState === xhr.DONE) {
 		if (xhr.status === 200) {
-			console.info(csvJSON(xhr.responseText));
+			console.log(xhr.responseText);
+			buildCards(csvJSON(xhr.responseText));
 		}
 	}
 };
